665919b77c49662ee01f691fb7016f111ca667eb
Unknown <nparche@web.de>
2016-12-08 00:43:49 +0100

    Vorbereitung_BP_ready_BS_FORMELN

---
+++ Matlab/GNP3_Aktiv_RC_Filter_BP.m
@@ -0,0 +1,46 @@
+%% BP
+
+% Parameter
+NAME = 'Bandpass';
+R0 = 3.32E3;
+Ra = 3.32E3;
+Rc = 16.5E3;
+R = 10E3;
+C = 10E-9;
+
+% HP Grenzfrequenz und Plots
+
+Tau = R*C;
+Vmax = R0/Ra;
+b1 = (Tau^2);
+a1 = (R0/Rc)*(Tau);
+
+p = (((a1^2)-(2*b1)-(2*(a1^2)*(Vmax^2)))/b1^2);
+q = (1/(b1^2));
+
+x1 =  -(p/2)-sqrt(((p/2)^2)-q);
+x2 =  -(p/2)+sqrt(((p/2)^2)-q);
+
+wg1 = +sqrt(x1);
+wg2 = -sqrt(x1);
+wg3 = +sqrt(x2);
+wg4 = -sqrt(x2);
+
+fg1 = wg1/(2*pi());
+fg2 = wg2/(2*pi());
+fg3 = wg3/(2*pi());
+fg4 = wg4/(2*pi());
+
+
+sys1 = tf([Vmax*a1 0], [(b1) (a1) 1]);
+h = bode(sys1);
+h=bodeplot(sys1);
+p = getoptions(h);
+p.FreqUnits = 'Hz';
+p.Grid = 'On';
+setoptions(h, p);
+title(sprintf('Bode Diagram - %s', NAME))
+xlim([10E1 1E4]);
+
+damp1_sys1 = 20*log10(bode(sys1, wg1))
+damp2_sys1 = 20*log10(bode(sys1, wg3))
